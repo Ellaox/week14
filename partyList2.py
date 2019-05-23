9fd6a66134826229a879edbe40a7ed64e592d0a6
Ellaox <40073721+Ellaox@users.noreply.github.com>
2019-05-23 12:04:04 +0100

    Create partyList2.py

---
+++ partyList2.py
@@ -0,0 +1,27 @@
+# partyList2.py 
+# a program to introduce list methods
+# to maintain a list of names to invite
+# to a party.
+# D.H. March 2005
+
+print "\t\tThis program allows you to maintain"
+print "\t\ta list of names of people to invite"
+print "\t\tto a party\n"
+
+names = []   # create an empty list
+
+choice = "z" # initialize choice with any value that will
+             # set the while loop to true
+             
+========================================================================
+
+Python 2.7.6 (default, Nov 10 2013, 19:24:24) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+		This program allows you to maintain
+		a list of names of people to invite
+		to a party
+
+>>> 
+>>> 
